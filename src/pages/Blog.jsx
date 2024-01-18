b800ffe58357056c32e7572766d8657834cdb508
Nacho <italiyankina@gmail.com>
2024-01-18 18:59:36 +0000

    add input

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { useFetch } from "../useFetch";
 export const Blog = () => {
+    let [searchParams, setSearchParams] = useSearchParams();
 
     const { data, error, loading } = useFetch("https://rickandmortyapi.com/api/character");
 
@@ -8,14 +9,27 @@ export const Blog = () => {
         return (<h1> Buscando el Morty adecuado... </h1>);
     }
     if (error) {
-        return (<h1> La pistola de portales no funciona... </h1>)
+        return (<h1> La pistola de portales no funciona... </h1>);
     }
 
     console.log(data.results);
 
+
+    const handleChange = (e) => {
+        setSearchParams({ [e.target.name]: e.target.value });
+    }
+
     return (
         <>
             <h1> Blog - Elije tu personaje favorito </h1>
+            <input
+                type="text"
+                name=""
+                onChange={handleChange}
+                className="form-control my-3"
+                alt="Buscador"
+                value={searchParams.get("filter") || ""}>
+            </input>
             <ul className="list-group">
                 {
                     data.results.map(element => {
