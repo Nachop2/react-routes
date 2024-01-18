80a304a8f80be49c18f98f0c4fb3c60d66eb7be1
Nacho <italiyankina@gmail.com>
2024-01-18 18:52:50 +0000

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Inicio } from "./pages/Inicio";
+import { Contacto } from "./pages/Contacto";
+import { Blog } from "./pages/Blog";
+import { BlogCharacter } from "./pages/BlogCharacter";
+import { NotFound } from "./pages/NotFound";
+import { Layout } from "./layouts/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <h1></h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+
+
+          <Route element={<Inicio />} path="/"></Route>
+          <Route element={<Contacto />} path="/contacto"></Route>
+          <Route element={<Blog />} path="/blog"></Route>
+          <Route element={<BlogCharacter />} path="/blog/:id"></Route>
+          <Route element={<NotFound />} path="/*"></Route>
+        </Route>
+
+      </Routes>
+    </>
   );
 }
 
