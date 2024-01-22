770ed3595f58e82de3064cdd491a10522d04d7b7
Nacho <italiyankina@gmail.com>
2024-01-22 19:41:47 +0000

    Filtered data 2

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,32 +1,35 @@
 import { Link, useSearchParams } from "react-router-dom";
 import { useFetch } from "../useFetch";
+import { useState } from "react";
 export const Blog = () => {
-    let [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
 
+    const [filteredData, setData] = useState([]);
     const { data, error, loading } = useFetch("https://rickandmortyapi.com/api/character");
 
     if (loading) {
         return (<h1> Buscando el Morty adecuado... </h1>);
     }
+
     if (error) {
         return (<h1> La pistola de portales no funciona... </h1>);
     }
-
-
-    let filteredData = data.results.filter((e) => e);
+    console.log(filteredData);
+    //Find way to add condition that executes once, and does not loop infinitely
+    if (filteredData) {
+        console.log("AAAAAAAAAAAAA");
+        setData(data.results);
+    }
 
     const handleChange = (e) => {
         setSearchParams({ filter: e.target.value });
-
-        console.log(data.results);
         let term = e.target.value;
-        console.log(term);
-        term = term.replace("(", "\\(");
-        term = term.replace(")", "\\)");
+
+        //term = term.replace("(", "\\(");
+        //term = term.replace(")", "\\)");
         // Regex filtering
         let regexSearch = new RegExp("(" + term + ")", "ig");
-        filteredData = data.results.filter((e) => e.name.match(regexSearch));
-        console.log(filteredData);
+        setData(data.results.filter((e) => e.name.match(regexSearch)));
     }
 
     return (
@@ -42,7 +45,7 @@ export const Blog = () => {
             </input>
             <ul className="list-group">
                 {
-                    data.results.map(element => {
+                    filteredData.map(element => {
                         return (
                             <Link className="list-group-item"
                                 key={element.id}
