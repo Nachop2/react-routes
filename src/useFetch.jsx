ac806f3695ff9aec426edc839ab823bd7e99a214
Nacho <italiyankina@gmail.com>
2024-01-24 19:43:34 +0000

    update useFetch

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -1,34 +1,41 @@
 import { useCallback, useEffect, useState } from "react";
 
-export const useFetch = (url) =>{
+export const useFetch = (url, cent = true) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
-    console.log("useFetch");
-    const fetchData = useCallback( async() =>{
+    const fetchData = useCallback(async () => {
         setLoading(true);
-        try{
-            const res = await fetch(url);
-            if(!res.ok){
-                throw Error("Error al usar la api");
+        if (cent) {
+            try {
+                const res = await fetch(url);
+                if (!res.ok) {
+                    throw Error("Error al usar la api");
+                }
+                const data = await res.json();
+                setData(data);
+            } catch (error) {
+                console.log(error);
+                setData([]);
+                setError(error.message);
+            } finally {
+                setLoading(false);
             }
-            const data = await res.json();
-            setData(data);
-        }catch(error){
-            console.log(error);
-            setData([]);
-            setError(error.message);
-        }finally{
+        }else{
             setLoading(false);
         }
-    },[]);
+
+    }, []);
+
+
+    console.log("useFetch");
+
 
     useEffect(() => {
         console.log("useEffect");
         fetchData();
-    },[]);
+    }, []);
 
-    return { data, loading, error}
-    
+    return { data, loading, error }
+
 };
