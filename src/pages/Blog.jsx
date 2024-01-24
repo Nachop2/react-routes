25a35cf13c2b45f3879f724b3573dc9990f9dc51
Nacho <italiyankina@gmail.com>
2024-01-24 18:05:01 +0000

    Fix filtering

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,12 +1,12 @@
 import { Link, useSearchParams } from "react-router-dom";
 import { useFetch } from "../useFetch";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export const Blog = () => {
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const [filteredData, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
     const { data, error, loading } = useFetch("https://rickandmortyapi.com/api/character");
-
+    
     if (loading) {
         return (<h1> Buscando el Morty adecuado... </h1>);
     }
@@ -14,21 +14,9 @@ export const Blog = () => {
     if (error) {
         return (<h1> La pistola de portales no funciona... </h1>);
     }
-    console.log(filteredData);
-    //Find way to add condition that executes once, and does not loop infinitely
-    if (false) {
-        console.log("AAAAAAAAAAAAA");
-        setData(data.results);
-    }
 
     const handleChange = (e) => {
         setSearchParams({ filter: e.target.value });
-        let term = e.target.value;
-        //term = term.replace("(", "\\(");
-        //term = term.replace(")", "\\)");
-        // Regex filtering
-        let regexSearch = new RegExp("(" + term + ")", "ig");
-        setData(data.results.filter((e) => e.name.match(regexSearch)));
     }
 
     return (
@@ -44,7 +32,8 @@ export const Blog = () => {
             </input>
             <ul className="list-group">
                 {
-                    filteredData.map(element => {
+                    
+                    data.results.filter((e) => e.name.match(new RegExp("(" + searchParams.get("filter") + ")", "ig"))).map(element => {
                         return (
                             <Link className="list-group-item"
                                 key={element.id}
