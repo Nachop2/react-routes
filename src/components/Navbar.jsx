1b0eed92a512131f4d1aa1f3ed0a816aa57bb647
Nacho <italiyankina@gmail.com>
2024-01-24 18:07:41 +0000

    Navbar cleanup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,6 @@ export const Navbar = () => {
                 <Link className="btn btn-outline-primary" to="/">Inicio</Link>
                 <Link className="btn btn-outline-primary" to="/contacto">Contacto</Link>
                 <Link className="btn btn-outline-primary" to="/blog">Blog</Link>
-                <Link className="btn btn-outline-primary" to="/"></Link>
-                <Link className="btn btn-outline-primary" to="/"></Link>
             </div>
         </div>
     );
