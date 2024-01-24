33d14fa055d4cf1f25f05a0ab28af481b31034e1
Nacho <italiyankina@gmail.com>
2024-01-24 19:43:20 +0000

    Add CharacterContext

--- src/App.js
+++ src/App.js
@@ -8,24 +8,33 @@ import { Blog } from "./pages/Blog";
 import { BlogCharacter } from "./pages/BlogCharacter";
 import { NotFound } from "./pages/NotFound";
 import { Layout } from "./layouts/Layout";
+import { createContext } from 'react';
+import { useState } from 'react';
+export const CharactersContext = createContext();
 
 function App() {
+
+  const [charactersList, setCharactersList] = useState(null);
+  console.log(charactersList)
   return (
     <>
-      <Navbar></Navbar>
-      <h1></h1>
-      <Routes>
-        <Route path="/" element={<Layout />}>
+      <CharactersContext.Provider value={{ charactersList, setCharactersList }}>
+
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Layout />}>
 
 
-          <Route element={<Inicio />} path="/"></Route>
-          <Route element={<Contacto />} path="/contacto"></Route>
-          <Route element={<Blog />} path="/blog"></Route>
-          <Route element={<BlogCharacter />} path="/blog/:id"></Route>
-          <Route element={<NotFound />} path="/*"></Route>
-        </Route>
+            <Route element={<Inicio />} path="/"></Route>
+            <Route element={<Contacto />} path="/contacto"></Route>
+            <Route element={<Blog />} path="/blog"></Route>
+            <Route element={<BlogCharacter />} path="/blog/:id"></Route>
+            <Route element={<NotFound />} path="/*"></Route>
+          </Route>
 
-      </Routes>
+        </Routes>
+      </CharactersContext.Provider>
+
     </>
   );
 }
